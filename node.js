14b8cde212427be4d9f385100ecb10da65e2acee
Rezax <abdulbaarihindustani@gmail.com>
2024-04-28 10:21:53 +0530

    Create Board Function 3rd Commit

---
+++ node.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+    
+    createBoard(32);
+    console.log("HI");
+})
+
+function createBoard(size){
+    let board = document.querySelector(".board");
+
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let numDivs = size * size;
+
+    for(let i = 0; i < numDivs; i++){
+        let div = document.createElement("div");
+        board.insertAdjacentElement("beforeend", div);
+        div.style.backgroundColor = "yellow";
+    }
+}
